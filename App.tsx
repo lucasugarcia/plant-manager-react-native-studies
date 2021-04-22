e958f33317858deefd4ee80fce32364d9a3d00ac
Lucas Garcia <lucasugarcia@gmail.com>
2021-04-22 19:26:39 -0300

    Criação do projeto, alterando somente o texto da tela inicial

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+	return (
+		<View style={style.container}>
+			<Text>NLW#5</Text>
+		</View>	
+	);
+}
+
+const style = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+});
