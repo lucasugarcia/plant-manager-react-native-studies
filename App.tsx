b1e142436f4e51795d3bb1ef3ad0576f6a9a988a
Lucas Garcia <lucasugarcia@gmail.com>
2021-04-23 20:04:34 -0300

    Implementação final da tela inicial, utilizando definitivamente fontes de texto externas e melhorando o layout

--- App.tsx
+++ App.tsx
@@ -4,12 +4,12 @@ import { Welcome } from './src/pages/Welcome';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
 export default function App() {
-	const [ fontsLoaded ] = useFonts({
-			Jost_400Regular, 
-			Jost_600SemiBold
+	const [fontsLoaded] = useFonts({
+		Jost_400Regular,
+		Jost_600SemiBold
 	});
 
-	if(!fontsLoaded)
+	if (!fontsLoaded)
 		return <AppLoading />
 
 	return (
