2ab1d79c13d5582c2c6a47b5065b6cb31defc29f
Lucas Garcia <lucasugarcia@gmail.com>
2021-04-24 18:21:19 -0300

    Melhorias na tela de seleção das plantas, utilizando filtros para exibir somente as plantas de determinado ambiente

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -7,25 +7,63 @@ import fonts from '../styles/fonts';
 import { EnvironmentButton } from '../components/EnvironmentButton';
 import { FlatList } from 'react-native-gesture-handler';
 import api from '../services/api';
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
 
 interface EnvironmentProps {
     key: string;
     title: string;
 }
 
+interface PlantProps {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string]
+    frequency: {
+        times: number;
+        repeat_every: string;
+    }
+}
+
 export function PlantSelect() {
 
     const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
+    const [plants, setPlants] = useState<PlantProps[]>([]);
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+    const [environmentSelected, setEnvironmentSelected] = useState('all')
+
+    function handleEnvironmentSelected(environment: string) {
+        setEnvironmentSelected(environment);
+
+        if(environment === 'all')
+            return setFilteredPlants(plants);
+
+        const filtered = plants.filter(plant => 
+            plant.environments.includes(environment)
+        );
+
+        setFilteredPlants(filtered);
+    }
 
     useEffect(() => {
         async function fetchEnvironment() {
-            const { data } = await api.get('plants_environments');
-            setEnvironments([{key:'all', title: 'Todos'}, ...data]);
+            const { data } = await api.get('plants_environments?_sort=title&_order=asc');
+            setEnvironments([{ key: 'all', title: 'Todos' }, ...data]);
         }
 
         fetchEnvironment();
     }, []);
 
+    useEffect(() => {
+        async function fetchPlants() {
+            const { data } = await api.get('plants?_sort=name&_order=asc');
+            setPlants(data);
+        }
+
+        fetchPlants();
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -41,15 +79,30 @@ export function PlantSelect() {
 
             <View>
                 <FlatList
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
                     data={environments}
                     renderItem={({ item }) => (
                         <EnvironmentButton
                             title={item.title}
+                            active={item.key === environmentSelected}
+                            onPress={() => handleEnvironmentSelected(item.key)}
                         />
                     )}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
                     contentContainerStyle={styles.environmentList}
+                    keyExtractor={(item, index) => item.title.toString()}
+                />
+            </View>
+
+            <View style={styles.plants}>
+                <FlatList
+                    data={filteredPlants}
+                    renderItem={({ item }) => (
+                        <PlantCardPrimary data={item} />
+                    )}
+                    keyExtractor={(item, index) => item.id.toString()}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={2}
                 />
             </View>
         </View>
@@ -83,5 +136,10 @@ const styles = StyleSheet.create({
         paddingBottom: 5,
         marginLeft: 32,
         marginVertical: 32
+    },
+    plants: {
+        flex: 1,
+        paddingHorizontal: 32,
+        justifyContent: 'center'
     }
 });
