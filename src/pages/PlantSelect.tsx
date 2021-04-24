801f04f2e28c462b3b2d65debc830f69462702d4
Lucas Garcia <lucasugarcia@gmail.com>
2021-04-24 15:31:11 -0300

    Desenvolvimento de um header para as telas, com nome e foto do usuário

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
+import { Header } from '../components/Header';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function PlantSelect() {
     return (
         <View style={styles.container}>
-            <Text>Selecione sua planta</Text>
+            <View style={styles.header}>
+                <Header />
+                <Text style={styles.title}>
+                    Em qual ambiente
+                </Text>
+                <Text style={styles.subtitle}>
+                    você quer colocar sua planta?
+                </Text>
+            </View>
         </View>
     );
 };
@@ -14,8 +24,22 @@ export function PlantSelect() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: colors.background,
-        justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: colors.background
+    },
+    header: {
+        paddingHorizontal: 30
+    },
+    title: {
+        fontSize: 17,
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginTop: 15
+    },
+    subtitle: {
+        fontFamily: fonts.text,
+        fontSize: 17,
+        lineHeight: 20,
+        color: colors.heading
     }
 });
