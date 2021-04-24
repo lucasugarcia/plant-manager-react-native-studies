3ea5b50e1536f454fa860a81491c0acd2954e0a2
Lucas Garcia <lucasugarcia@gmail.com>
2021-04-24 16:32:10 -0300

    Implementado um componente para o botão dos ambientes, utilizando flatlist para agrupar estes novos componentes na tela, com informações a partir de um json em uma fake api (json-server)

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import { Header } from '../components/Header';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
+import { EnvironmentButton } from '../components/EnvironmentButton';
+import { FlatList } from 'react-native-gesture-handler';
+import api from '../services/api';
+
+interface EnvironmentProps {
+    key: string;
+    title: string;
+}
 
 export function PlantSelect() {
+
+    const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
+
+    useEffect(() => {
+        async function fetchEnvironment() {
+            const { data } = await api.get('plants_environments');
+            setEnvironments([{key:'all', title: 'Todos'}, ...data]);
+        }
+
+        fetchEnvironment();
+    }, []);
+
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -16,6 +37,20 @@ export function PlantSelect() {
                 <Text style={styles.subtitle}>
                     você quer colocar sua planta?
                 </Text>
+            </View>
+
+            <View>
+                <FlatList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    data={environments}
+                    renderItem={({ item }) => (
+                        <EnvironmentButton
+                            title={item.title}
+                        />
+                    )}
+                    contentContainerStyle={styles.environmentList}
+                />
             </View>
         </View>
     );
@@ -41,5 +76,12 @@ const styles = StyleSheet.create({
         fontSize: 17,
         lineHeight: 20,
         color: colors.heading
+    },
+    environmentList: {
+        height: 40,
+        justifyContent: 'center',
+        paddingBottom: 5,
+        marginLeft: 32,
+        marginVertical: 32
     }
 });
