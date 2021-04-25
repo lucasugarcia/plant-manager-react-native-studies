c0558ceec8fb6f44c90068b3d1a4df66aaa9074c
Lucas Garcia <lucasugarcia@gmail.com>
2021-04-24 22:25:48 -0300

    Carregando o nome do usuário na tela principal conforme o que está armazenado no dispositivo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native'; 
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import colors from '../styles/colors';
 import userImg from '../assets/Lucas.jpg';
 import fonts from '../styles/fonts';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function Header() {
+    const [userName, setUserName] = useState<string>();
+
+    useEffect(() => {
+        async function loadStorageUserName() {
+            const user = await AsyncStorage.getItem('@plantmanager:user');
+            setUserName(user || '');
+        }
+
+        loadStorageUserName()
+
+    }, [userName]);
+
     return(
         <View style={styles.container}>
             <View>
                 <Text style={styles.greeting}>Olá,</Text>
-                <Text style={styles.userName}>Lucas</Text>
+                <Text style={styles.userName}>{userName}</Text>
             </View>
             <Image source={userImg} style={styles.image}/>
         </View>
