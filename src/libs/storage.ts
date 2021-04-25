ffb070addd71cb3f7b7c330ab96e7ada62a7d78c
Lucas Garcia <lucasugarcia@gmail.com>
2021-04-25 12:09:20 -0300

    Refactoring da tela MyPlants e PlantSave e início da implementação de swipe para excluir uma planta cadastrada

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -7,15 +7,16 @@ export interface PlantProps {
     about: string;
     water_tips: string;
     photo: string;
-    environments: [string]
+    environments: [string];
     frequency: {
         times: number;
         repeat_every: string;
-    },
+    };
+    hour: string;
     dateTimeNotification: Date;
 }
 
-interface StoragePlantProps {
+export interface StoragePlantProps {
     [id: string]: {
         data: PlantProps;
     }
